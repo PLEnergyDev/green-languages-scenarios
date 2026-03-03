650e581fd459deca1004f75be0671cf7dd0ffaef
dragos-ionescu-aau <dragosi@cs.aau.dk>
2026-03-03 14:24:03 +0100

    remove build artifact

--- build/rust/polynomial-evaluation/release/release/deps/program-aa1d1b35db0303d5.d
+++
@@ -1,5 +0,0 @@
-/home/dragos/Documents/green-languages-scenarios/./build/rust/polynomial-evaluation/release/release/deps/program-aa1d1b35db0303d5.d: main.rs
-
-/home/dragos/Documents/green-languages-scenarios/./build/rust/polynomial-evaluation/release/release/deps/program-aa1d1b35db0303d5: main.rs
-
-main.rs:
